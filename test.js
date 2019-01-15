51e629af24a66c0f31b0f2bb87a91552af2064f1
judell <jon@jonudell.info>
2019-01-14 16:13:39 -0800

    wip

--- test.js
+++ test.js
@@ -2,38 +2,78 @@ const puppeteer = require('puppeteer')
 
 const CRX_PATH = '/users/jon/onedrive/h/1.93/'
 
-function currentPage(browser) {
-    return new Promise( (resolve,reject) => {
-      browser.targets()
-        .then( targets => {
-          for (let i = 0, I = targets.length; i < I; ++i) {
-            target = targets[i]
-            let page = target.page()
-              .then( page => {
-                console.log(page.url())
-                if (page) {
-                  resolve(page)
-                }
-              })
-          }
-        })
-        .catch ( _ => {
-          reject(null)      
-        })
+function delay(seconds) {
+  return new Promise( resolve => setTimeout(resolve, seconds * 1000))
+}
 
-    })
+async function waitSeconds(seconds) {
+  await delay(seconds)
   }
+
 
 puppeteer.launch({
   headless: false, // extensions only supported in full chrome.
   args: [
     `--disable-extensions-except=${CRX_PATH}`,
     `--load-extension=${CRX_PATH}`,
-//    '--user-agent=PuppeteerAgent'
+    '--remote-debugging-port=9222',
+    '-- enable-devtools-experiments'
   ]
 }).then(async browser => {
+  await waitSeconds(2) // give extension time to load
   let pages = await browser.pages()
-  console.log(pages)
-  //let page = await browser.targets()[browser.targets().length-1].page();
-  //await page.goto('http://jonudell.net/h/power-of-habit.pdf')
+  let page = pages[1] // 0 is the about page, 1 is the welcome page with h extension loaded
+  let targets = browser.targets()
+  let target = targets.filter (target => target._targetInfo.url.startsWith('https://hypothes.is'))[0]
+  //console.log('target\n', target)
+  const client = await target.createCDPSession()
+  await client.send('Runtime.enable')
+
+  function evaluateExpression(client, expression, contextId) {
+    return new Promise( (resolve,reject) => {
+      client.send('Runtime.evaluate', { 
+        //awaitPromise: false,
+        expression: expression, 
+        contextId: contextId,
+        //returnByValue: true,
+        includeCommandLineAPI: true,        
+        //generatePreview: true,
+        //throwOnSideEffect: true,
+        timeOut: 500,
+        //userGesture: false
+      })
+      .then(r => {
+        resolve(r.result)
+      })
+      .catch(e => {
+        reject(e)
+      })
+    })
+  }
+
+  client.on('Runtime.executionContextCreated', msg => { 
+    let context = msg.context
+    let contextId = context.id
+    console.log(contextId)
+    let expression = "document.querySelectorAll('hypothesis-highlight')"
+    if (contextId != 3 ) {
+      return
+    }
+    evaluateExpression(client, expression, contextId)
+      .then(r => {
+        client.send('Runtime.getProperties', {objectId: r.objectId, ownProperties: true})
+          .then(r => {
+            console.log(r.result[0])
+          })
+      })
+      .catch(e => {
+        console.log(e)
+      })
+  })
+
+  await waitSeconds(2)
+
+  await client.send('Page.navigate', { url: "http://jonudell.net/h/ee12.pdf" })
+
 })
+  
