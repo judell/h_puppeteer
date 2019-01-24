ca8c4f4c6b8b4590c8ce6c62bd2e8dc8d7234478
judell <jon@jonudell.info>
2019-01-24 15:24:48 -0800

    let per-page nav settle (otherwise 'execution context destroyed')

--- test.js
+++ test.js
@@ -33,19 +33,20 @@ async function callSearchApi(testUrl) {
       })
     })
   }
-  
 async function runPdfTest(testUrl) {
     let browser = await  puppeteer.launch({
-        headless: false, // extensions only supported in full chrome.
-        args: [
-          `--disable-extensions-except=${CRX_PATH}`,
-          `--load-extension=${CRX_PATH}`,
-          '--remote-debugging-port=9222',
-          // '--enable-devtools-experiments' # useful for sniffing the chrome devtools protocol
-        ]
-      })
+      headless: false, // extensions only supported in full chrome.
+      args: [
+        `--disable-extensions-except=${CRX_PATH}`,
+        `--load-extension=${CRX_PATH}`,
+        '--remote-debugging-port=9222',
+        // '--enable-devtools-experiments' # useful for sniffing the chrome devtools protocol
+      ]
+    })
+
     await waitSeconds(2) // give extension time to load
-    let pages = await browser.pages()
+
+    let pages = await browser.pages()    
     let page = pages[1] // 0 is the about page, 1 is the welcome page with h extension loaded
     const client = await page.target().createCDPSession()
     await client.send('Runtime.enable')
@@ -60,14 +61,15 @@ async function runPdfTest(testUrl) {
 
     for (let i = 1; i <= 4; i++) {
       await client.send('Page.navigate', { url: `${testUrl}#${i}` })
+      await waitSeconds(3) // let nav settle before running code in the page
       const results = await page.evaluate(() => { // this function runs in the browser, is not debuggable here
-        document.querySelector('button[name=sidebar-toggle]').click()
+        //document.querySelector('button[name=sidebar-toggle]').click()
         let nodes = Array.from(document.querySelectorAll('hypothesis-highlight'))
         let highlights = nodes.map(node => {return {text: node.innerHTML, class: node.getAttribute('class')}})
         nodes = nodes.filter(node => { return node.innerText != 'Loading annotations…' })
         return Promise.resolve({highlights: highlights, nodeCount: nodes.length, highlightCount: highlights.length})
       })
-      let anchored = {}
+      var anchored = {}
       results.highlights.forEach(highlight => {
           // ids are sent from the sidebar, and added to the classname by annotator, 
           // in order to coalesce highlights that span dom nodes
@@ -78,7 +80,6 @@ async function runPdfTest(testUrl) {
           anchored[annoId] += highlight.text
       })
     }
-
 
     let apiResults = await(callSearchApi(testUrl))
     browser.close()
@@ -95,6 +96,6 @@ async function runTestOnAllPdfUrls() {
 }
 
 runTestOnAllPdfUrls()
- .then(r => {
-   console.log(r)
+ .then(results => {
+   console.log(JSON.parse(results))
  })
